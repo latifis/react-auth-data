562a0d7906faed9e4a2579aa0c617c04b10d9cc8
latif <latifirfansyah@gmail.com>
2024-03-08 11:01:27 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import './App.css';
-import { Login } from "./component/Login";
-import { Register } from "./component/Register";
+import "./App.css";
+import Login from "./component/Login";
+import Register from "./component/Register";
 import { Home } from "./component/Home"; // Import halaman Home
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./component/NotFound";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState("login");
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
 
   return (
-    <div className="App">
-      {currentForm === "login" && <Login onFormSwitch={toggleForm} />}
-      {currentForm === "register" && <Register onFormSwitch={toggleForm} />}
-      {currentForm === "home" && <Home />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/component/NotFound.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const NotFound = () => {
+  return <h1>NOT FOUND</h1>;
+};
+
+export default NotFound;
