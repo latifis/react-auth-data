c3715f25b2b4e8d1365fd23900ebd5cb637b2792
latif <latifirfansyah@gmail.com>
2024-03-08 11:06:29 +0700

    add route for home aand form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import Login from "./component/Login";
 import Register from "./component/Register";
-import { Home } from "./component/Home"; // Import halaman Home
+import Home from "./component/Home"; // Import halaman Home
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./component/NotFound";
+import Form from "./component/Form";
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
@@ -18,6 +19,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/form" element={<Form />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </Router>
---
+++ src/component/Form.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  return (
+    <div className="home-container">
+      <h2>Form</h2>
+    </div>
+  );
+};
+
+export default Form;
